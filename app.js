5d47697dfe85c3c96696199a906fe028f48b7ad1
KYOYA OGASAWARA <64949271+KYOYA-OGA@users.noreply.github.com>
2022-06-16 16:54:11 +0700

    add cors, not found route.

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const morgan = require('morgan');
 const { errorHandler } = require('./middlewares/error');
+const cors = require('cors');
 require('express-async-errors');
 require('dotenv').config();
 
 require('./db');
 const userRouter = require('./routes/user');
+const { handleNotFound } = require('./utils/helper');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 app.use('/api/user', userRouter);
+
+app.use('/*', handleNotFound);
 
 app.use(errorHandler);
 
