d1913feb5d50a152b3905b5d3e07056aab8bac9e
KYOYA OGASAWARA <64949271+KYOYA-OGA@users.noreply.github.com>
2022-06-18 12:15:48 +0700

    setup actor model

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ require('dotenv').config();
 
 require('./db');
 const userRouter = require('./routes/user');
+const actorRouter = require('./routes/actor');
 const { handleNotFound } = require('./utils/helper');
 
 const app = express();
@@ -15,6 +16,7 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 app.use('/api/user', userRouter);
+app.use('/api/actor', actorRouter);
 
 app.use('/*', handleNotFound);
 
--- middlewares/validator.js
+++ middlewares/validator.js
@@ -26,6 +26,16 @@ exports.validatePassword = [
     .withMessage('Password must be between 8 and 30 characters long!'),
 ];
 
+exports.actorInfoValidator = [
+  check('name').trim().not().isEmpty().withMessage('Actor name is missing!'),
+  check('about').trim().not().isEmpty().withMessage('About is required field'),
+  check('gender')
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage('Gender is required field'),
+];
+
 exports.validate = (req, res, next) => {
   const error = validationResult(req).array();
   if (error.length) {
