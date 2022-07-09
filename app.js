34213cb1512dbeaa7e39ea172c90b41bf9b87f8d
KYOYA OGASAWARA <kyoyaoga@gmail.com>
2022-07-09 19:08:29 +0700

    fixed deployment issues

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use('/*', handleNotFound);
 
 app.use(errorHandler);
 
-app.listen(8000, () => {
-  console.log('listening on port 8000');
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
 });
